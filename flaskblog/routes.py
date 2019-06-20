b50efe1538c923392cb661861f801b0e9c7204a5
shivratangupta <shivratangupta18@gmail.com>
2019-06-21 00:44:10 +0530

    add account page and next page request

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, url_for, flash, redirect
-from flask_login import login_user, current_user, logout_user
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
@@ -56,8 +56,9 @@ def login():
 	if(form.validate_on_submit()):
 		user = User.query.filter_by(email = form.email.data).first()
 		if user and bcrypt.check_password_hash(user.password, form.password.data):
-			login_user(user, remember = form.remember.data) 
-			return redirect(url_for('home'))
+			login_user(user, remember = form.remember.data)
+			next_page = request.args.get('next')
+			return redirect(next_page) if next_page else redirect(url_for('home'))
 		else:
 			flash('Login Unsuccessful. Please check email and password', 'danger')
 	return render_template('login.html', title = 'Login', form = form)
@@ -70,5 +71,6 @@ def logout():
 
 
 @app.route("/account")
+@login_required
 def account():
 	return render_template('account.html', title = 'Account')
